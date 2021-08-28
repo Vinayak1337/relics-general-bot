c5a74a7bb9dd1fc5c5490d612cd80347a9114bf2
Vinayak <34209962+Vinayak1337@users.noreply.github.com>
2021-08-29 02:29:23 +0530

    Cutting tokens

--- data/keys/settings.js
+++ data/keys/settings.js
@@ -1,9 +1,9 @@
-exports.token = 'NjY0NzQyMDU1NDI3NDQwNjUy.XhbfSA.aUhvSdRWXVUPNRet15ju1fQTqYs';
-exports.token2 = 'Mzc2NDE0MzE1MDY3OTk4MjE4.Wf3xJg.tg7hrRap-cc-e8II52J1uaFHba8';
+exports.token = 'NjY0NzQyMDU1NDI3NDQwNjUy.XhbfSA.aU************************';
+exports.token2 = 'Mzc2NDE0MzE1MDY3OTk4MjE4.Wf3xJg.tg7hrRap*********';
 exports.owners = [
 	'359223782747144192',
 	'329184236206686210',
 	'149626954113351680',
 ];
 exports.uri =
-  'mongodb+srv://vinayak:tcBezoOlESXc7FWX@brawlstarsdatabases.73h9i.mongodb.net/RelicsIndia?retryWrites=true&w=majority';
+  'mongodb+srv://vinayak:******@brawlstarsdatabases.73h9i.mongodb.net/RelicsIndia?retryWrites=true&w=majority';
